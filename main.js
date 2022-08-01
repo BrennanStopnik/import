fc64b71e4e1c2283845723d613354fb74743ef42
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-08-01 12:57:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 // Using Import will get the function from the other page and let us run it here.
 
 import { add } from "./notMain.js";
+import { add2 } from "./notMain.js";
+import { subtract } from "./notMain.js";
 
 add(5,6);
+console.log(add2(5,6));
+
+console.log(`Subtracting b from a is: ${subtract(6,5)}`)
